f750feb963c3ce6db4f3a31f71ae310819caaf2a
Emily <>
2020-12-05 23:59:39 -0500

    next and hasNext

--- Range.java
+++ Range.java
@@ -19,14 +19,22 @@ public class Range implements IntegerSequence{
 
   //When current is no longer a valid element in the range, it should return false.
   public boolean hasNext(){
-    return true;
+    if(current >= start && current < end){
+      return true;
+    }
+    return false;
   }
 
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by 1.
   //e.g.  if current is 5. This will make current 6, and return 5.
   public int next(){
-    return 0;
+    if(hasNext() == false){
+      throw new NoSuchElementException(current + " is not in range of " + start + " - " + end);
+    }
+    int temp = current;
+    current ++;
+    return temp;
   }
 
 }
